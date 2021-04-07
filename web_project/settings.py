ad05d9903539c9ac8dd4629e5645ba074adbd317
Christopher DiStasio <cadistasio@gmail.com>
2021-04-06 22:09:55 -0500

    Stubs database connection

--- web_project/settings.py
+++ web_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': str(BASE_DIR / 'dreamsourcer'),
+        'HOST': "/tmp/mysql.sock",
+        'PORT': '',
+        'USER': 'user',
+        'PASSWORD': 'password'
     }
 }
 
